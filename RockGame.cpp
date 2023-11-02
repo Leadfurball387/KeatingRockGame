193b389c15584dcde87fa9be8d97b2f3d5b0f2e6
Leadfurball387 <leadfurball387@gmail.com>
2023-11-01 23:25:06 -0700

    Created main game file. Assigns variables and prints inverted pyramid of 'O's.

---
+++ RockGame.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	
+	// User input to select which row to take a rock (O) from
+	int row{};
+
+	// # of rocks in each row
+	int row1{ 5 };
+	int row2{ 4 };
+	int row3{ 3 };
+
+	// Inverted pyramid of 'O's, representing rocks
+	std::cout << "1. O   O   O   O   O\n2.   O   O   O   O\n3.     O   O   O";
+
+}
